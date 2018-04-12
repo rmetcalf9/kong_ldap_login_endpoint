5cdddf5badb1548b8e79b5eb6bda5cd00a9104aa
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-12 14:45:58 +0100

    Auth header decoding

--- app/src/loginAPI.py
+++ app/src/loginAPI.py
@@ -1,11 +1,41 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from werkzeug.exceptions import Unauthorized, BadRequest
+import re
+from base64 import b64decode
 
 login_api_blueprint = Blueprint('login_api_blueprint',__name__)
 
 @login_api_blueprint.route('/', methods=['GET'])
 def login():
-  raise BadRequest('Not Implemented')
+  authHeader = request.headers.get('Authorization')
+  if authHeader is None:
+    raise BadRequest('Missing Authorization Header')
+  authHeader = authHeader.strip()
+  base64Str = ''
+  if re.match('basic ', authHeader, re.I):
+    base64Str = authHeader[6:]
+  elif re.match('ldap ', authHeader, re.I):
+    base64Str = authHeader[5:]
+  if base64Str == '':
+    raise BadRequest('Invalid Authorization Header')
+  try:
+    decoded = b64decode(base64Str).decode()
+  except:
+    raise BadRequest('Invalid Authorization Header (encoding)')
+
+  arr = decoded.split(':')
+  if len(arr) != 2:
+    raise BadRequest('Invalid Authorization Header (value)')
+  username = arr[0]
+  password = arr[1]
+
+  print(username)
+  print(password)
+
+
+
+  #raise BadRequest('Not Implemented')
+  raise Exception('TODO')
 
 def registerAPI(appObj):
   appObj.flaskAppObject.register_blueprint(login_api_blueprint, url_prefix='/login')
--- app/test/test_loginAPI.py
+++ app/test/test_loginAPI.py
@@ -1,10 +1,38 @@
 from TestHelperSuperClass import testHelperAPIClient
+from base64 import b64encode
+
+from requests._internal_utils import to_native_string
+def _basic_auth_str(prefix, username, password):
+    if isinstance(username, str):
+        username = username.encode('latin1')
+
+    if isinstance(password, str):
+        password = password.encode('latin1')
+
+    authstr = prefix + to_native_string(
+        b64encode(b':'.join((username, password))).strip()
+    )
+
+    return authstr
 
 class test_loginAPI(testHelperAPIClient):
   def test_loginNoAuthCredentials(self):
     result = self.testClient.get('/login/')
     self.assertEqual(result.status_code, 400)
 
+  def test_loginBadAuthHeader(self):
+    result = self.testClient.get('/login/',headers={'Authorization': 'BadAuthHeader'})
+    self.assertEqual(result.status_code, 400)
+
+  def test_loginAuthNotBase64(self):
+    username = 'TestUser'
+    password = 'TestPassword'
+    combined = username + ':' + password
+    result = self.testClient.get('/login/',headers={'Authorization': 'Basic ' + combined})
+    self.assertEqual(result.status_code, 400)
+
   def test_loginBadCredentials(self):
-    result = self.testClient.get('/login/')
+    username = 'TestUser'
+    password = 'TestPassword'
+    result = self.testClient.get('/login/',headers={'Authorization': _basic_auth_str('Basic ', username, password)})
     self.assertEqual(result.status_code, 401)
